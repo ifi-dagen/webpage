4f969773fd5a097ecc74e8c2e11a58aff7908429
Nikolas Martin <nikolhm@1x-193-157-236-47.uio.no>
2019-10-07 23:31:54 +0200

    Change countdown date to 9:30 on dagen

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 10, 2019 00:00:00").getTime();
+var countDownDate = new Date("Oct 10, 2019 09:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
