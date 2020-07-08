b11efd8eb8c52449befae3c5e7fad89cac9687a9
kariesta <kariesta@ifi.uio.no>
2020-07-08 23:31:21 +0200

    Hiding pricing information for now. Only hiding the info by css made the info still available by inspecting the html.

--- src/Bedrift.js
+++ src/Bedrift.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import './bedrift.css';
-import Visitkort from './Visitkort'
+import Visitkort from './Visitkort';
+import Prisinfo from './Prisinfo';
 
 
-class Bedrift extends Component {
+export default class Bedrift extends Component {
     render(){
         return (
             <div className="bedriftbase ">
@@ -43,7 +44,7 @@ class Bedrift extends Component {
                 </div>
 
                 <div className ="mailingliste hidden">
-                    <h1>MAILINGLISTE - null backend atm!</h1>
+                    <h1>MAILINGLISTE - null backend atm, men knappen dukker opp her når den er klar!</h1>
                     <div className="form-group">
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Skriv e-postadresse her..." />
                         <a className="btn btn-lg btn-header btn_bedrift" href="#?">Meld din interesse</a>
@@ -68,71 +69,10 @@ class Bedrift extends Component {
                         registrerer dere under påmeldingskjema om dere ønsker å delta i år.
                     </p>
                 </div>
-                <div className="priser ">
-                    <h1>PRISER OG PAKKER</h1>
-                    <p>info om pakker, i år kan vi droppe v-boksene-v</p>
-                    <div className="row ">
-                        <h2> Stand-pakker for <text color="#08A6E4">dagen@ifi</text> </h2>
-                    </div>
 
-                    <div className="row ">
-
-                        <div className="col-lg-3 stand_pakke stand_pakke_1">
-                            <div className="stand_pakke_inner">
-                                <h2 className="svart_overskrift">STANDPLASS</h2>
-                                <h5 className="blue">pris: 35 000,-</h5>
-
-                                <br/><b>Pakken inkluderer:</b>
-                                <br/>- Standplass på ca 4m2
-                                <br/>- Bord og to barkrakker
-                                <br/>- Trådløst internett
-                                <br/>- Strøm (to kontakter)
-                                <br/>- Logo i informasjonsbrosjyren for dagen@ifi
-                                <br/>- Tilgang til bedriftslounge med mat og drikke på dagtid
-                                <br/>- Mulighet for å holde foredrag/lyntale
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 stand_pakke stand_pakke_2">
-                            <div className="stand_pakke_inner">
-
-                                <h2 className="svart_overskrift">SPONSOR</h2>
-                                <h5 className="blue">pris: 55 000,-</h5>
-
-                                <br/><b>Pakken inkluderer:</b>
-                                <br/>- Alt som er inkludert i standakken
-                                <br/>- Navn og logo fremhevet på vår nettside og trykksaker
-                                <br/>- Intervjurom
-                                <br/>- 1. Prioritet på valg av standplass
-                                <br/>- Garantert et foredrag eller en lyntale
-                                <br/>- 10 bonger for bruk på kvelden
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 stand_pakke stand_pakke_3">
-                            <div className="stand_pakke_inner">
-
-                                <h2 className="svart_overskrift">HOVEDSPONSOR</h2>
-                                <h5 className="blue">pris: 100 000,-</h5>
-
-                                <br/><b>Pakken inkluderer:</b>
-                                <br/>- Standplass på ca 4m2
-                                <br/>- Bord og to barkrakker
-                                <br/>- Trådløst internett
-                                <br/>- Strøm (to kontakter)
-                                <br/>- Logo i informasjonsbrosjyren for dagen@ifi
-                                <br/>- Tilgang til bedriftslounge med mat og drikke på dagtid
-                                <br/>- Mulighet for å holde foredrag/lyntale
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
+				<Prisinfo className="hidden"/>
             </div>
         );
     }
 
 }
-
-export default Bedrift;
