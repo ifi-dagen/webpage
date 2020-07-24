d881fd16b9ed7df0c76e6fc8389968b421b52184
kariesta <kariesta@ifi.uio.no>
2020-07-24 11:24:20 +0200

    StengeBanner som rendres på passende tidspunkt

--- src/Stander.js
+++ src/Stander.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './stander.css';
 import Modal from './components/modal.js';
+import StengeBanner from './components/stengeBanner.js';
 import stand_info from './data/stand_info.js'
 
 export default class Stander extends Component {
@@ -25,15 +26,15 @@ export default class Stander extends Component {
     this.showModal(stand_info["dag1"][e.target.id]);
   };
 
-  stander = (stand_info) => {
-    //TODO: differansier basert på hvilke dager det er
-    return stand_info["dag1"].map((bedrift, index) => {
+  stander = (dag,open,closed_message) => {
+
+    return stand_info[dag].map((bedrift, index) => {
       return (
         <div className="button-card-container"
              id={index} key={index}>
           <button className="button-card"
                   id={index} key={index}
-                  onClick={(e)=>{this.bedriftpop(e)}}>
+                  onClick={open? (e)=>{this.bedriftpop(e)}: ()=>{}}>
             <img id={index}
                  className="button-img"
                  src={ require("./img/logoer/"+bedrift.logonavn) }
@@ -44,13 +45,51 @@ export default class Stander extends Component {
     })
   }
 
+  display = (now) => {
+    console.log(now);
+    //for testing: edit the following variables as you please!
+    const stand_start = new Date(2020, 6, 24, 11, 20, 0, 0);
+    const stand_stop = new Date(2020, 6, 24, 11, 30, 0, 0);
+    const stand_start2 = new Date(2020, 6, 24, 11, 40, 0, 0);
+    const stand_stop2 = new Date(2020, 6, 24, 11, 50, 0, 0);
+    let display = (<div>displaied</div>);
+    if (now <stand_start){
+      display = (<div>Ikke begynt</div>);
+    } else if (now <stand_stop){
+      display = (this.stander("dag1",true));
+    } else if (now <stand_start2){
+      const useStyles = ({
+            root: {
+              position: 'absolute',
+            },
+            backdrop: {
+              position: 'absolute',
+            },
+          });
+      const classes = useStyles;
+
+      display = (<div>
+                  <StengeBanner className={classes.root} disablePortal fullwidth melding="Pause frem til kl 13:00, se foredrag!"/>
+                  <div className="stand-container">
+                    {this.stander("dag1",false)}
+                  </div>
+                </div>);
+    } else if (now <stand_stop2){
+      display = (this.stander("dag2",true," "));
+    } else {
+      display = (<div>Ferdig</div>);
+    }
+    return(
+      display
+    )
+  }
 
   render(){
       return (
           <div className="standbase">
 			        <p>Her kommer stander!</p>
               <Modal show={this.state.show} handleClose={this.hideModal} bedrift={this.state.currBedrift}/>
-              {this.stander(stand_info)}
+              {this.display(new Date())}
           </div>
       );
   }
