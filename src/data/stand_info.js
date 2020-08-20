6524760b5647fdf2708f33bf2cfeb34abfb07cc6
kariesta <kariesta@ifi.uio.no>
2020-08-20 16:51:04 +0200

    ice breakers og tags oppdatert

--- src/data/stand_info.js
+++ src/data/stand_info.js
@@ -21,21 +21,21 @@ const stand_info =
       logonavn: "dnb-example.jpg",
       zoomlink:"https://uio.zoom.us/",
       stillinger: [{tekst: "Vi søker devOps-utvikler med 12 års kyberneteserfaring!", link: "https://www.jobbnorge.no/ledige-stillinger/stilling/188610/instituttleder-ved-institutt-for-teknologisystemer"}],
-      ice_breakers: [],
+      ice_breakers: ["prosjektledelse","bedriftansvar"],
       konkurranser: [{tekst: "Vi har en artig konkuranse!", link:"https://www.dnb.no/privat/sparing-og-investering/spare/konkurranse-festivalsommer.html"}],
       foredrag: [],
       tags: ["UX","SIKKERHET"]
     },
     {
       bedriftnavn: "Eksempel med ey-logo",
-      beskrivelse: "Et hjem for oss et hjem for deg (dette eksempelet har litt mindre innhold)",
+      beskrivelse: "Et innovativt og fremoverlent sted. (dette eksempelet har litt mindre innhold)",
       logonavn: "ey-example.png",
       zoomlink:"https://zoom.us/",
       stillinger: [{tekst: "Bli fulltid fullstacker høsten 2021!", link: "https://www.jobbnorge.no/ledige-stillinger/stilling/188610/instituttleder-ved-institutt-for-teknologisystemer"}],
-      ice_breakers: ["Hotell","TV"],
+      ice_breakers: [],
       konkurranser: [],
       foredrag: [],
-      tags: ["LEDELSE","TV","HOTELL"]
+      tags: ["LEDELSE"]
 
     }
   ],
