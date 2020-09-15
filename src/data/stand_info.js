402a07e364ec25e84d5603d254b1c331844cd488
kariesta <kariesta@ifi.uio.no>
2020-09-15 18:18:23 +0200

    more stand info

--- src/data/stand_info.js
+++ src/data/stand_info.js
@@ -95,11 +95,11 @@ const stand_info =
         tags: ["",""]
       },
     "Fremtind Forsikring AS": {
-        bedriftnavn: "",
+        bedriftnavn: "Fremtind Forsikring AS",
         beskrivelse: "",
         logonavn: "icon_1.png",
         zoomlink:"https://uio.zoom.us/j/63761070770",
-        stillinger: [{tekst: "", link: ""}],
+        stillinger: [{tekst: "Summer Internship 2021", link: "https://candidate.hr-manager.net/ApplicationInit.aspx?cid=1889&ProjectId=144150&DepartmentId=20575&MediaId=4630"}],
         konkurranser: [{tekst: "", link:""}],
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
@@ -124,7 +124,7 @@ const stand_info =
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
       },
-    "Kolonial.no AS ": {
+    "Kolonial.no AS": {
         bedriftnavn: "",
         beskrivelse: "",
         logonavn: "icon_1.png",
@@ -145,21 +145,22 @@ const stand_info =
         tags: ["",""]
       },
     "Netcompany": {
-        bedriftnavn: "",
-        beskrivelse: "",
+        bedriftnavn: "Netcompany",
+        beskrivelse: "Netcompany ble grunnlagt i år 2000 og har hovedkontor i København i Danmark. I dag er vi et internasjonalt selskap med over 2500 medarbeidere i seks land. Netcompany er et rent IT-selskap som leverer forretningskritiske IT-prosjekter som hjelper kundene med digital forandring via digitale plattformer, kjernesystemer og infrastruktursystemer. Vi vet at det er medarbeiderne våre som er nøkkelen til at vi kan beholde vår posisjon som bransjens ledende og raskest voksende IT-konsulenthus. Derfor har vi skapt en forretningsmodell der vi hele tiden fokuserer på å utvikle medarbeiderne våre til å bli de beste i bransjen. Det betyr at karrieren din er minst like viktig for oss som den er for deg.",
         logonavn: "icon_1.png",
+        video: "https://www.youtube.com/watch?v=T9e93Olxu4w",
         zoomlink:"https://uio.zoom.us/j/62811861036",
-        stillinger: [{tekst: "", link: ""}],
+        stillinger: [{tekst: "Nyutdannet utvikler med oppstart 2021", link: "https://www.netcompany.com/no/Components/Job-Positions/Student,-c-,-Nyutdannet-utvikler-med-oppstart-august-2021"}, {tekst: "Summer Internship 2021 Utvikler", link: "https://www.netcompany.com/no/Components/Job-Positions/Student,-c-,-Summer-internship-2021---Utvikler"}, {tekst: "Summer Internship 2021 UX Designer", link: "https://www.netcompany.com/no/Components/Job-Positions/Student,-c-,-Summer-internship-2021---UX-designer"}, {tekst: "Talent Network 2021", link: "https://www.netcompany.com/no/Components/Job-Positions/Student,-c-,-Talent-Network-2021"}],
         konkurranser: [{tekst: "", link:""}],
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
       },
     "Netlight AS": {
-        bedriftnavn: "",
+        bedriftnavn: "Netlight AS",
         beskrivelse: "",
-        logonavn: "icon_1.png",
+        logonavn: "Netlight.png",
         zoomlink:"https://uio.zoom.us/j/65612593008",
-        stillinger: [{tekst: "", link: ""}],
+        stillinger: [{tekst: "Summer Internship 2021", link: "https://jobs.lever.co/netlight/783adfbe-6a59-4a16-b4b2-df64d44fba87"}],
         konkurranser: [{tekst: "", link:""}],
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
@@ -175,19 +176,21 @@ const stand_info =
         tags: ["",""]
       },
     "Posten og Bring": {
-        bedriftnavn: "",
+        bedriftnavn: "Posten og Bring",
         beskrivelse: "",
-        logonavn: "icon_1.png",
+        logonavn: "Posten_Norge.png",
+        video: "http://www.kaltura.com/tiny/kfbaa",
         zoomlink:"https://uio.zoom.us/j/69158490010",
-        stillinger: [{tekst: "", link: ""}],
+        stillinger: [{tekst: "Konserntrainee i Posten og Bring 2021", link: "https://candidate.hr-manager.net/ApplicationInit.aspx?cid=1641&ProjectId=144904&DepartmentId=18956&MediaId=5"}],
         konkurranser: [{tekst: "", link:""}],
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
       },
     "Schibsted": {
-        bedriftnavn: "",
+        bedriftnavn: "Schibsted",
         beskrivelse: "",
-        logonavn: "icon_1.png",
+        logonavn: "Schibsted.png",
+        video: "https://youtu.be/RBskvdqt5dA",
         zoomlink:"https://uio.zoom.us/j/63837890960",
         stillinger: [{tekst: "", link: ""}],
         konkurranser: [{tekst: "", link:""}],
@@ -204,10 +207,10 @@ const stand_info =
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
       },
-    "SpareBank 1 Utvikling ": {
-        bedriftnavn: "",
-        beskrivelse: "",
-        logonavn: "icon_1.png",
+    "SpareBank 1 Utvikling": {
+        bedriftnavn: "SpareBank 1 Utvikling",
+        beskrivelse: "SpareBank 1 Utvikling er de 14 SpareBank 1-bankenes felles selskap for utvikling, drift, innkjøp, merkevare og mye annet spennende. 600 dyktige medarbeidere skaper og forvalter de beste digitale løsningene, tjenestene og brukeropplevelsene i bransjen - for over 1 million kunder. Vi går på jobb hver dag for å gjøre hverdagsøkonomien enklere og bedre for folk og bedrifter over hele landet. Vi vektlegger læring og kompetanse, frihet og trivsel i arbeidshverdagen. Dette gjør SpareBank 1 Utvikling til et spennende sted å jobbe, og et godt sted å være. Spør oss om: sommerjobb, fagdag, hackaton, fadderordning og nativeutvikling",
+        logonavn: "SpareBank_1.png",
         zoomlink:"https://uio.zoom.us/j/66307450744",
         stillinger: [{tekst: "", link: ""}],
         konkurranser: [{tekst: "", link:""}],
@@ -225,8 +228,8 @@ const stand_info =
         tags: ["",""]
       },
     "Systek AS": {
-        bedriftnavn: "",
-        beskrivelse: "",
+        bedriftnavn: "Systek AS",
+        beskrivelse: "Jobb med de skarpeste hodene i et selskap som er eid av de ansatte og i stadig vekst!",
         logonavn: "icon_1.png",
         zoomlink:"https://uio.zoom.us/j/68298550948",
         stillinger: [{tekst: "", link: ""}],
@@ -236,11 +239,12 @@ const stand_info =
       },
     "Visma Consulting AS": {
         bedriftnavn: "",
-        beskrivelse: "",
+        beskrivelse: "Hvert år rekrutterer vi dyktige nyutdannede som er engasjert og nysgjerrig på ny teknologi! Vi jobber for å ligge i front når det gjelder ny teknologi og utvikler hver dag samfunnskritiske løsninger. Vi leverer konsulenttjenester i form av IT-rådgivning, systemutvikling og applikasjonsforvaltning til både private og offentlige virksomheter.",
         logonavn: "icon_1.png",
+        video: "https://www.youtube.com/watch?v=sIg1bXYQ_KU&feature=youtu.be",
         zoomlink:"https://uio.zoom.us/j/68346630888",
-        stillinger: [{tekst: "", link: ""}],
-        konkurranser: [{tekst: "", link:""}],
+        stillinger: [{tekst: "IT-konsulent i Nytt Krutt", link: "https://vismaconsulting.recman.no/job.php?job_id=160140"},{tekst: "Sommerjobb som IT-konsulent", link: "https://vismaconsulting.recman.no/job.php?job_id=160321"}],
+        konkurranser: [{tekst: "Kahoot i vårt Zoom-rom kl 12.00 med premie! Bli med :-)", link:""}],
         foredrag: [{tekst: "", link: ""}],
         tags: ["",""]
       },
@@ -250,8 +254,8 @@ const stand_info =
         logonavn: "icon_1.png",
         zoomlink:"https://uio.zoom.us/j/69056279057",
         stillinger: [{tekst: "", link: ""}],
-        konkurranser: [{tekst: "", link:""}],
-        foredrag: [{tekst: "", link: ""}],
+        konkurranser: [{tekst: "Answer a few questions about Vitec and win a gift card from Komplett worth 5000nok", link: "https://forms.office.com/Pages/ResponsePage.aspx?id=e1R1xImDvUmIRzwO8Lnhgevb-ZXAbwxGg6vsx6QAJ7RUQkk5NURMQUI2MFFLNlNNMVE0RkxVRklFOC4u"}],
+        foredrag: [{tekst: "", link:""}],
         tags: ["",""]
       }
   },
@@ -267,8 +271,8 @@ const stand_info =
       tags: ["",""]
     },
     "Bekk": {
-      bedriftnavn: "",
-      beskrivelse: "",
+      bedriftnavn: "Bekk",
+      beskrivelse: "Bekk er et norsk selskap som lykkes på grunn av mennesker som brenner for faget sitt. Vi jobber med strategi, design og teknologi og tror på tverrfaglige team. Hos oss får du muligheten til å utvikle løsninger som hjelper bedrifter med å nå sine mål innen digitalisering og nyskaping.\nLyst til å jobbe i Bekk?\nHovedrunden med rekruttering til fast jobb og sommerjobb er om høsten, men vi er alltid åpne for søknader fra de rette kandidatene!",
       logonavn: "icon_1.png",
       zoomlink:"https://uio.zoom.us/j/63181529688",
       stillinger: [{tekst: "", link: ""}],
@@ -403,7 +407,7 @@ const stand_info =
       foredrag: [{tekst: "", link: ""}],
       tags: ["",""]
     },
-    "Experis Ciber ": {
+    "Experis Ciber": {
       bedriftnavn: "",
       beskrivelse: "",
       logonavn: "icon_1.png",
@@ -424,9 +428,10 @@ const stand_info =
       tags: ["",""]
     },
     "If Skadeforsikring": {
-      bedriftnavn: "",
+      bedriftnavn: "If Skadeforsikring",
       beskrivelse: "",
       logonavn: "icon_1.png",
+      video: "https://youtu.be/EHc9r89EUUU",
       zoomlink:"https://uio.zoom.us/j/64462476032",
       stillinger: [{tekst: "", link: ""}],
       konkurranser: [{tekst: "", link:""}],
@@ -444,21 +449,22 @@ const stand_info =
       tags: ["",""]
     },
     "Kantega AS": {
-      bedriftnavn: "",
-      beskrivelse: "",
+      bedriftnavn: "Kantega AS",
+      beskrivelse: "Kantega er et IT-selskap med rundt 165 stolte medarbeidere, i Oslo, Trondheim og Bergen. Vi eier vårt eget selskap. Det gjør noe med oss og forholdet til kundene våre og løsningene vi lager: Fantastiske IT-løsninger, de som forbedrer livene til hver av oss.",
       logonavn: "icon_1.png",
+      video: "https://www.youtube.com/watch?v=ocwnns57cYQ&t=1s",
       zoomlink:"https://uio.zoom.us/j/66579280932",
-      stillinger: [{tekst: "", link: ""}],
-      konkurranser: [{tekst: "", link:""}],
+      stillinger: [{tekst: "Søk sommerjobb hos oss", link: "https://sommerjobb.kantega.no/jobs/929530-spennende-sommerjobb-i-tverrfaglig-team"}],
+      konkurranser: [{tekst: "Delta i vår konkurranse og vinn flotte premier", link:"https://www.kantega.no/dagen-ifi-2020"}],
       foredrag: [{tekst: "", link: ""}],
       tags: ["",""]
     },
     "Lime Technologies Norway AS": {
-      bedriftnavn: "",
+      bedriftnavn: "Lime Technologies Norway AS",
       beskrivelse: "",
       logonavn: "icon_1.png",
       zoomlink:"https://uio.zoom.us/j/61602017949",
-      stillinger: [{tekst: "", link: ""}],
+      stillinger: [{tekst: "European Trainee Program", link: "https://jobs.lime-technologies.com/trainee/"}],
       konkurranser: [{tekst: "", link:""}],
       foredrag: [{tekst: "", link: ""}],
       tags: ["",""]
@@ -474,9 +480,9 @@ const stand_info =
       tags: ["",""]
     },
     "Norsk Regnesentral": {
-      bedriftnavn: "",
+      bedriftnavn: "Norsk Regnesentral",
       beskrivelse: "",
-      logonavn: "icon_1.png",
+      logonavn: "Norsk_regnesentral.png",
       zoomlink:"https://uio.zoom.us/j/69153286477",
       stillinger: [{tekst: "", link: ""}],
       konkurranser: [{tekst: "", link:""}],
