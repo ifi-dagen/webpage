44dbc4ab5740542e077638a44f93a94bb21be3ea
siljosaurus <silje@ignitas.no>
2020-09-16 19:23:23 +0200

    Added etter module to standområde

---
+++ src/pages/standomraade/etter.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Etter = () => {
+    return (
+        <Wrapper>
+            <h1>dagen@ifi er over, vi sees i 2021.</h1>
+            <h3>Vi vil gjerne høre hva du synes om årets karrieredag!</h3>
+            <Evaluering />
+        </Wrapper>
+    )
+}
+
+
+const Evaluering = () => {
+    return (
+        <Link href="https://www.dagenatifi.no">
+            <Icon className="fas fa-heart"> </Icon>
+            <br />
+            Gå til evaluering
+        </Link>
+    )
+}
+
+
+const Link = styled.a`
+align-items: center;
+padding: 1em;
+`
+
+const Icon = styled.i`
+font-size: 10em;
+color: deepskyblue;
+transition: color 0.30s;
+cursor: pointer;
+
+${Link}:hover & {
+  color: dodgerblue;
+}
+`
+
+const Wrapper = styled.div`
+    display: grid;
+    text-align: center;
+    padding: 2em;
+    
+    h1 {
+        color: black;
+    }
+
+`
+
+export default Etter;
