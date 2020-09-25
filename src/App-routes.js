ad4677eb22a99979b6cf1ccaf1c81b884ba981b2
kariesta <kariesta@ifi.uio.no>
2020-09-25 13:38:02 +0200

    egen link til etter-modus

--- src/App-routes.js
+++ src/App-routes.js
@@ -7,6 +7,7 @@ import Frivillig from "./pages/frivillig/Frivillig";
 import Front from './pages/front/Front';
 import Error from './pages/error/Error';
 import Stander from './pages/standomraade/Stander';
+import Etter from './pages/standomraade/etter';
 import AlleStander from './pages/standomraade/alleStander';
 import BedriftStand from './pages/bedriftstand/BedriftStand';
 import Studenter from "./pages/studenter/studenter";
@@ -58,6 +59,11 @@ class Routes extends Component {
           exact
           path="/torsdag"
           render={prop => <Stander {...prop} store={this.props.store} testmode={false} dag="dag1"/>}
+        />
+        <Route
+          exact
+          path="/etter"
+          render={prop => <Etter {...prop} store={this.props.store}/>}
         />
         <Route
           render={prop => <Error {...prop} store={this.props.store} />}
