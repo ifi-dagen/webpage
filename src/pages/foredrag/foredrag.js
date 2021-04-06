6d6a14c342dfbdb8a7278333282d2b314ee9f83a
eivindgg <eivindgg@ifi.uio.no>
2021-04-06 19:45:03 +0200

    started setting up skeleton

--- src/pages/foredrag/foredrag.js
+++ src/pages/foredrag/foredrag.js
@@ -1,9 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-const Foredrag = () => {
-    return <Wrapper>heheh</Wrapper>;
+const Foredrag = (props) => {
+    return <h1>Heisann sweisann</h1>;
+    // <Wrapper>
+    //     <Time>{props.time}</Time>;<Company>{props.company}</Company>;
+    //     <Title>{props.title}</Title>;<About>{props.about}</About>;
+    //     <Place>{props.place}</Place>;<Speaker>{props.speaker}</Speaker>;
+    // </Wrapper>;
 };
+
+const Time = styled.div``;
+
+const Company = styled.div``;
+
+const Title = styled.div``;
+
+const About = styled.div``;
+
+const Place = styled.div``;
+
+const Speaker = styled.div``;
 
 const Wrapper = styled.div`
     padding: 0rem 5rem;
