0fbb72d2ed26428c8ee75952b87b62c43b9d9d83
eivindgg <eivindgg@ifi.uio.no>
2021-04-08 16:14:58 +0200

    statisk foredrag utkast

---
+++ src/data/foredrag_info.js
@@ -0,0 +1,44 @@
+const aapningslink = 'https://www.instagram.com/dagenatifi/'
+const foredrag_info = [
+    {
+        id: 0,
+        tittel: 'bedrift1',
+        type: 'Annet',
+        startTime: new Date(2021, 4, 30, 9, 0, 0, 0),
+        endTime: new Date(2020, 4, 30, 12, 30, 0, 0),
+        companyName: 'Kripos',
+        about:
+            'Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology',
+        link:
+            'https://uio.zoom.us/webinar/register/com?fbclid=IwAR32ym3xFAOX0QVCTt6j4H2ZpzfAkeOIDW0uwEE7H9Gc9c7Bjbll9j1Afno',
+        speaker: 'NAME',
+        alt_tekst: '',
+    },
+    {
+        id: 1,
+        title: 'bedrift1',
+        startTime: new Date(2020, 4, 30, 9, 0, 0, 0),
+        endTime: new Date(2020, 4, 30, 13, 30, 0, 0),
+        companyName: 'Kripos',
+        about:
+            'Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology',
+        link:
+            'https://uio.zoom.us/webinar/register/com?fbclid=IwAR32ym3xFAOX0QVCTt6j4H2ZpzfAkeOIDW0uwEE7H9Gc9c7Bjbll9j1Afno',
+        speaker: 'NAME',
+        alt_tekst: '',
+    },
+    {
+        id: 2,
+        tittel: 'bedrift1',
+        type: 'Annet',
+        startTime: new Date(2020, 4, 30, 9, 0, 0, 0),
+        endTime: new Date(2020, 4, 30, 14, 30, 0, 0),
+        companyName: 'Kripos',
+        about:
+            'Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology Artificial Intelligence using Big Data Machine Learning Blockchain Technology',
+        link:
+            'https://uio.zoom.us/webinar/register/com?fbclid=IwAR32ym3xFAOX0QVCTt6j4H2ZpzfAkeOIDW0uwEE7H9Gc9c7Bjbll9j1Afno',
+        speaker: 'NAME',
+        alt_tekst: '',
+    },
+]
--- src/pages/foredrag/foredrag.js
+++ src/pages/foredrag/foredrag.js
@@ -1,93 +1,184 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import '../kontakt/kontakt.css'
 
 const Foredrag = (props) => {
+    const history = useHistory()
+
     return (
+        /*
+        TODO:
+        * Make skelton for design 
+        * Create company group grid
+        * Feed info through props
+        * Link time to time view for program
+        * 
+        */
+
         <Wrapper>
             <Title>
-                <h1>Artificial Big Data Science Blockchain </h1>
+                <h1>
+                    Artificial Intelligence using Big Data Machine Learning
+                    Blockchain Technology{' '}
+                </h1>
             </Title>
-            <Time>
-                <h3>13:00 - 14:00</h3>
+
+            <Time onClick={() => history.push('/program#3')}>
+                {props.startTime} - {props.endTime}
             </Time>
+            <SpeakerInfo companyName={props.companyName} />
             <About>
-                <p>
-                    {" "}
-                    Heisann sweisann Heisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                    Heisann sweisann Heisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                    Heisann sweisann Heisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                    Heisann sweisannHeisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                    Heisann sweisann Heisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                    Heisann sweisann Heisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                    Heisann sweisann Heisann sweisann Heisann sweisann Heisann
-                    sweisann Heisann sweisann Heisann sweisann Heisann sweisann
-                </p>
+                <h3>Om foredraget</h3> Artificial Intelligence using Big Data
+                Machine Learning Blockchain Technology Artificial Intelligence
+                using Big Data Machine Learning Blockchain Technology Artificial
+                Intelligence using Big Data Machine Learning Blockchain
+                Technology Artificial Intelligence using Big Data Machine
+                Learning Blockchain Technology Artificial Intelligence using Big
+                Data Machine Learning Blockchain Technology Artificial
+                Intelligence using Big Data Machine Learning Blockchain
+                Technology Artificial Intelligence using Big Data Machine
+                Learning Blockchain Technology Artificial Intelligence using Big
+                Data Machine Learning Blockchain Technology Artificial
+                Intelligence using Big Data Machine Learning Blockchain
+                Technology Artificial Intelligence using Big Data Machine
+                Learning Blockchain Technology Artificial Intelligence using Big
+                Data Machine Learning Blockchain Technology Artificial
+                Intelligence using Big Data Machine Learning Blockchain
+                Technology Artificial Intelligence using Big Data Machine
+                Learning Blockchain Technology Artificial Intelligence using Big
+                Data Machine Learning Blockchain Technology Artificial
+                Intelligence using Big Data Machine Learning Blockchain
+                Technology Artificial Intelligence using Big Data Machine
+                Learning Blockchain Technology{' '}
             </About>
+            <RerouteToLink
+                location={
+                    'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO'
+                }
+            />
         </Wrapper>
-    );
+    )
 
     {
         /* //     <Time>{props.time}</Time>;<Company>{props.company}</Company>;
     //     <Title>{props.title}</Title>;<About>{props.about}</About>;
     //     <Place>{props.place}</Place>;<Speaker>{props.speaker}</Speaker>; */
     }
-};
+}
 
-const Time = styled.div`
-    h3 {
-        border: 2px;
-        border-style: solid;
-        border-color: deepskyblue;
-        padding: 1em;
-        width: 14%;
-        font-weight: bold;
-        border-radius: 4px;
-    }
-`;
+const SpeakerInfo = ({ companyName, speakerName }) => {
+    companyName = 'Netcompany'
+    return (
+        <CompanyContainer>
+            <CompanyName>{companyName}</CompanyName>
+            <CompanyLogo>
+                {' '}
+                <img
+                    src={require(`./logo/${companyName}_logo.png`)}
+                    alt={`Bilde av ${companyName} sin logo`}
+                ></img>
+            </CompanyLogo>
+            <Speaker>
+                <img
+                    src={require(`./speaker/${companyName}_speaker.png`)}
+                    alt={`Bilde av ${speakerName}`}
+                ></img>
+                <br></br>
+                {speakerName}
+            </Speaker>
+        </CompanyContainer>
+    )
+}
 
-const Company = styled.div``;
+const RerouteToLink = ({ location }) => {
+    return (
+        <Place>
+            <a target="_blank" href={location}>
+                Se foredraget her!
+            </a>
+        </Place>
+    )
+}
 
 const Title = styled.div`
     h1 {
         color: black;
         line-height: 1;
     }
-`;
+`
+
+const CompanyContainer = styled.div`
+    display: grid;
+    grid-template-areas: 'companyName companylogo speakerinfo xxx 
+    ';
+`
+const CompanyLogo = styled.div`
+    max-width: 20vw;
+`
+const Speaker = styled.div`
+    color: deepskyblue;
+    grid-area: speakerinfo;
+    img {
+        max-width: 8vw;
+        max-height: 8vw;
+    }
+    justify-items: center;
+    text-align: center;
+`
+const CompanyName = styled.div`
+    font-size: 1.17em;
+    padding-left: 1em;
+    width: 50%;
+    font-weight: bold;
+    color: black;
+    grid-area: companyName;
+`
+const Time = styled.div`
+    font-size: 1.17em;
+    width: 14%;
+    font-weight: bold;
+    &:hover {
+        color: deepskyblue;
+    }
+    padding-left: 1em;
+`
 
 const About = styled.div`
-    p {
-        border: 2px;
-        border-style: solid;
-        border-color: deepskyblue;
-        padding: 1em;
+    padding: 0 1em 1em 1em;
+    line-height: 1.5;
+    h3 {
+        color: grey;
     }
-`;
+`
 
-const Place = styled.div``;
-
-const Speaker = styled.div``;
+const Place = styled.div`
+    a {
+        color: black;
+        font-size: 20px;
+        line-height: 1;
+        text-decoration: none;
+    }
+    a:hover {
+        color: deepskyblue;
+    }
+    padding: 1em;
+`
 
 const Wrapper = styled.div`
-    padding: 0rem 5rem;
+    display: grid;
+    grid-template columns: 1fr 2em 2em auto;
+    padding: 1em;
     margin: auto;
     width: 70%;
-    line-height: 1.5;
-`;
+`
 
-const Point = styled.p`
-    color: deepskyblue;
-    font-size: 20px;
-    line-height: 1;
-`;
+const Border = styled.div`
+    border: 2px;
+    border-style: solid;
+    border-color: deepskyblue;
+    padding: 1em;
+    border-radius: 8px;
+`
 
-const Footer = styled.div`
-    padding: 5rem;
-`;
-
-export default Foredrag;
+export default Foredrag
