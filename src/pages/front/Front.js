e8ccd5da264153792d541cbadfa0415adff8ef12
siljosaurus <silje@ignitas.no>
2021-04-09 15:16:30 +0200

    Removed styled component console warning in Frontpage and Studenter

--- src/pages/front/Front.js
+++ src/pages/front/Front.js
@@ -10,8 +10,6 @@ const Front = () => {
     <Div>
       <Container>
         <h1>Velkommen til digital ettermiddagen@ifi.</h1>
-        <h3>Vi gleder oss til å arrangere vårsemesterets digitale karrieredag for dere! For øyeblikket er nettsiden under litt oppussing, men følg oss på sosiale medier og ikke nøl med å ta kontakt med styret om det er noe du lurer på.</h3>
-        <DateParagraph />
         <h3 >Vil du være med i dagen@ifi som intern? <a href="https://forms.gle/JLYYu7rds4MNifKq9">Søk her!</a></h3>
       </Container>
 
@@ -33,19 +31,6 @@ const Front = () => {
   );
 }
 
-const DateParagraph = () => {
-  const Paragraph = styled.h2`
-    padding: 1rem;
-    color: deepskyblue;
-  `
-
-  return (
-    <Paragraph>
-      <i className="fas fa-calendar"></i> 30. April
-    </Paragraph>
-  )
-
-}
 
 const Visitkort2 = ({ peep }) => {
   return (
