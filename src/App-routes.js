fad2a05ba1f3b0fa82a257b964354ef9c512d89d
leann <leann@ifi.uio.no>
2021-04-12 18:00:10 +0200

    fikset merge

--- src/App-routes.js
+++ src/App-routes.js
@@ -1,70 +1,92 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import Program from "./pages/program/Program";
-import Bedrift from "./pages/bedrift/Bedrift";
-import Frivillig from "./pages/frivillig/Frivillig";
-import Front from "./pages/front/Front";
-import Error from "./pages/error/Error";
-import Stander from "./pages/standomraade/Stander";
-import BedriftStand from "./pages/bedriftstand/BedriftStand";
-import Studenter from "./pages/studenter/studenter";
-import { BedriftKomponent } from "./pages/bedrift/BedriftKomponent";
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Program from './pages/program/Program'
+import Bedrift from './pages/bedrift/Bedrift'
+import Front from './pages/front/Front'
+import Error from './pages/error/Error'
+import Stander from './pages/standomraade/Stander'
+import BedriftStand from './pages/bedriftstand/BedriftStand'
+import Studenter from './pages/studenter/studenter'
+import Styret from './components/styret'
+import Foredrag from './pages/foredrag/foredrag'
+import { BedriftKomponent } from './pages/bedrift/BedriftKomponent'
 
 class Routes extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" render={(prop) => <Front />} />
-        <Route
-          path="/kontakt"
-          render={(prop) => <Kontakt {...prop} store={this.props.store} />}
-        />
-        <Route
-          exact
-          path="/program"
-          render={(prop) => (
-            <Program {...prop} store={this.props.store} published={true} />
-          )}
-        />
-        <Route
-          exact
-          path="/for-bedrift"
-          render={(prop) => <Bedrift {...prop} store={this.props.store} />}
-        />
-        <Route exact path="/studenter" render={(prop) => <Studenter />} />
-        <Route
-          exact
-          path="/frivillig"
-          render={(prop) => <Frivillig {...prop} store={this.props.store} />}
-        />
-        <Route
-          exact
-          path="/stander"
-          render={(prop) => (
-            <Stander {...prop} store={this.props.store} testmode={true} />
-          )}
-        />
-        <Route
-          exact
-          path="/stander/:dag/:bedrift"
-          render={(prop) => <BedriftStand />}
-        />
-        <Route
-          exact
-          path="/bedrift"
-          render={(prop) => (
-            <BedriftKomponent
-              {...prop}
-              store={this.props.store}
-              testmode={false}
-            />
-          )}
-        />
-        <Route
-          render={(prop) => <Error {...prop} store={this.props.store} />}
-        />
-      </Switch>
-    );
-  }
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" render={(prop) => <Front />} />
+                <Route
+                    exact
+                    path="/program"
+                    render={(prop) => (
+                        <Program
+                            {...prop}
+                            store={this.props.store}
+                            published={true}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/for-bedrift"
+                    render={(prop) => (
+                        <Bedrift {...prop} store={this.props.store} />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/studenter"
+                    render={(prop) => <Studenter />}
+                />
+                <Route
+                    exact
+                    path="/foredrag"
+                    render={(prop) => (
+                        <Foredrag {...prop} store={this.props.store} />
+                    )}
+                />
+                <Route exact path="/styret" render={(prop) => <Styret />} />
+                <Route
+                    exact
+                    path="/stander"
+                    render={(prop) => (
+                        <Stander
+                            {...prop}
+                            store={this.props.store}
+                            testmode={true}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/stander/:dag/:bedrift"
+                    render={(prop) => (
+                        <BedriftStand
+                            {...prop}
+                            store={this.props.store}
+                            testmode={false}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/bedrift"
+                    render={(prop) => (
+                        <BedriftKomponent
+                            {...prop}
+                            store={this.props.store}
+                            testmode={false}
+                        />
+                    )}
+                />
+                <Route
+                    render={(prop) => (
+                        <Error {...prop} store={this.props.store} />
+                    )}
+                />
+            </Switch>
+        )
+    }
 }
-export default Routes;
+export default Routes
