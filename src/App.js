9aab35efedbc37aacd24e9100401cbeb5a01438d
leann <leann@ifi.uio.no>
2021-04-12 17:16:22 +0200

    Lagt symboler i egen fil under components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Routes from "./App-routes";
 import Header from "./components/header";
-import { BedriftKomponent } from "./pages/bedrift/BedriftKomponent";
 
 const App = () => (
   <>
--- src/pages/bedrift/BedriftKomponent.js
+++ src/pages/bedrift/BedriftKomponent.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import styled from "styled-components";
-import bedriftInfo from "./bedrift_info.json";
+import Bedrift from "./bedrift_info.json";
+import {
+  Instagram,
+  Webpage,
+  Facebook,
+  Zoom,
+  Linkedin,
+} from "../../components/symbols";
 
 export const BedriftKomponent = () => {
+  //Får fra bedrift kohort
+  const bedrift = Bedrift.DNB;
   return (
     <div
       style={{
@@ -17,24 +26,24 @@ export const BedriftKomponent = () => {
     >
       <Container>
         <h3 style={{ gridArea: "info", justifySelf: "center", padding: "1em" }}>
-          Standen er åpen mellom 12:00-13:00
+          Snakk med oss på Zoom mellom {bedrift.standtime[0]} -{" "}
+          {bedrift.standtime[1]}
         </h3>
         <img
-          src={require("../../img/logoer/knowit.png")}
+          src={require("../../img/logoer/" + bedrift.logo)}
           alt={"Logo"}
           style={{ gridArea: "Logo" }}
         ></img>
-        {/* <h1 style={{ gridArea: "Name" }}>DNB</h1> */}
-        <Link
-          href={bedriftInfo.zoom}
-          style={{ gridArea: "Zoom", justifySelf: "center" }}
+        <div
+          style={{ gridArea: "Zoom", justifySelf: "center", padding: "1em" }}
         >
-          <Icon className="fas fa-video"></Icon> Møt oss på zoom!
-        </Link>
+          <Zoom link={bedrift.zoom} text={"Møt oss på Zoom!"} />
+        </div>
+
         <h2 style={{ gridArea: "Title" }}>Hvem er vi?</h2>
-        <p style={{ gridArea: "Description" }}>{bedriftInfo.description}</p>
+        <p style={{ gridArea: "Description" }}>{bedrift.description}</p>
         <ReactPlayer
-          url={bedriftInfo.video}
+          url={bedrift.video}
           // width="426px"
           // height="240px"
           style={{
@@ -43,63 +52,41 @@ export const BedriftKomponent = () => {
             paddingBottom: "2em",
           }}
         />
-        <h2 style={{ gridArea: "TitleTalk" }}>Vi vil snakke med deg!</h2>
-        <p style={{ gridArea: "Talk" }}>{bedriftInfo.talk}</p>
+
         <div style={{ gridArea: "SoMe" }}>
-          <Facebook link={bedriftInfo.facebook} />
-          <Instagram link={bedriftInfo.instagram} />
-          <Linkedin link={bedriftInfo.facebook} />
-          <Webpage link={bedriftInfo.website} />
+          <Facebook link={bedrift.facebook} />
+          <Instagram link={bedrift.instagram} />
+          <Linkedin link={bedrift.facebook} />
+          <Webpage link={bedrift.website} />
         </div>
-        {/* <h3 style={{ gridArea: "CompetitionTitle" }}>Konkurranse</h3>
+
+        <h2 style={{ gridArea: "TitleTalk" }}>Vi vil snakke med deg!</h2>
+        <p style={{ gridArea: "Talk" }}>{bedrift.talk}</p>
+
         <p style={{ gridArea: "Competition" }}>
-          {bedriftInfo.competition.text}
-          <Link href={bedriftInfo.competition.link}></Link>
-        </p> */}
-        <h3 style={{ gridArea: "Questions" }}>5 raske</h3>
-        <div style={{ gridArea: "Answers" }}>
-          {bedriftInfo.answer.map((answer) => {
-            return <p>{answer}</p>;
+          <h3>Konkurranse</h3>
+          <a href={bedrift.competition.link}>{bedrift.competition.text}</a>
+        </p>
+
+        <div style={{ gridArea: "Questions" }}>
+          <h3>5 raske</h3>
+          {bedrift.answer.map((answer) => {
+            return <p key={answer}>{answer}</p>;
           })}
         </div>
+
         <h3 style={{ gridArea: "Work" }}>Våre spennende stillingsannonser</h3>
-        <Link style={{ gridArea: "Annonse" }} href={bedriftInfo.stilling}>
-          Kul ny jobb
-        </Link>
+        <div style={{ gridArea: "Annonse" }}>
+          {bedrift.positions.map((position) => {
+            return (
+              <a key={position} href={position.link}>
+                {position.name}
+              </a>
+            );
+          })}
+        </div>
       </Container>
     </div>
-  );
-};
-
-const Webpage = (props) => {
-  return (
-    <Link href={props.link} style={{ gridArea: "Webpage" }}>
-      <Icon className="fas fa-desktop"></Icon>
-    </Link>
-  );
-};
-
-const Facebook = (props) => {
-  return (
-    <Link href={props.link}>
-      <Icon className="fab fa-facebook"></Icon>
-    </Link>
-  );
-};
-
-const Linkedin = (props) => {
-  return (
-    <Link href={props.link}>
-      <Icon className="fab fa-linkedin"></Icon>
-    </Link>
-  );
-};
-
-const Instagram = (props) => {
-  return (
-    <Link href={props.link}>
-      <Icon className="fab fa-instagram"></Icon>
-    </Link>
   );
 };
 
@@ -111,27 +98,13 @@ const Container = styled.div`
     "info info info info"
     "Logo Logo Logo SoMe"
     ". Zoom Zoom Zoom"
-    "Questions . Title ."
-    "Answers . Description Description"
+    "Competition . Title ."
+    ". . Description Description"
+    "Questions . Description Description"
     ". . Description Description"
     ". . Video Video"
     ". . TitleTalk TitleTalk"
     ". . Talk Talk"
     ". . Work Work"
     ". . Annonse Annonse";
-`;
-
-const Link = styled.a`
-  align-items: flex-start;
-  padding: 1em;
-`;
-
-const Icon = styled.i`
-  font-size: 2rem;
-  color: black;
-  transition: color 0.25s;
-
-  ${Link}:hover & {
-    color: deepskyblue;
-  }
 `;
