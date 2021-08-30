a27c35cd72ecf2a3cf7fa30c6ede4729323ccc46
leann <leann@ifi.uio.no>
2021-08-30 11:17:12 +0200

    Deleted unused files

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,71 +1,20 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { useHistory as useHerstory } from 'react-router-dom'
-import ettermiddagen from '../img/ettermiddagen.svg'
-import ettermiddagenannenfarge from '../img/ettermiddagenannenfarge.svg'
-import { Facebook, Instagram, Linkedin } from './symbols'
+import React from 'react'
+import {Link} from "react-router-dom"
 
-
-export const Footer = () => {
-    const herstory = useHerstory()
-    const [logo, setLogo] = useState(ettermiddagen)
-    const toggleLogo = () => {
-        if (logo === ettermiddagen) {
-            setLogo(ettermiddagenannenfarge)
-        } else if (logo === ettermiddagenannenfarge) {
-            setLogo(ettermiddagen)
-        }
-    }
-    return (
-        <div>
-            <Wrapper>
-                <img
-                    onClick={() => herstory.push('/')}
-                    src={logo}
-                    alt="ettermiddagen@ifi logo"
-                    onMouseEnter={() => toggleLogo()}
-                    onMouseOut={() => toggleLogo()}
-                />
-                <span />
-                <Box>
-                    <Instagram link="http://www.instagram.com/dagenatifi"/>
-                    <Facebook link="http://www.facebook.com/dagenatifi"/>
-                    <Linkedin link="http://www.linkedin.com/dagenatifi"/>
-                </Box>
-            </Wrapper>
+const Newfooter = () => {
+    return(
+        <div >
+            <h5>Følg oss</h5>
+            <a href="http://www.instagram.com/dagenatifi"> Instagram</a> <br/>
+            <a href="http://www.linkedin.com/dagenatifi"> LinkedIn</a> <br/>
+            <a href="http://www.facebook.com/dagenatifi"> Facebook</a>
+            <h5>Les mer</h5>
+            <Link to={"/om-oss"}>Om foreningen</Link> <br/>
+            <p>Dagen er en frivillig studentforening med tilhørighet til Institutt for Informatikk, UiO. Vi skaper kontakt mellom studenter og næringslivet ved å arrangere to karrieredager i året. På høsten arrangeres dagen@ifi, og på våren ettermiddagen@ifi, begge finner sted på Ole-Johan Dahls hus.</p>
         </div>
     )
 }
 
-const Box = styled.div`
-    justify-self: right;
-    line-height: 2;
-`
 
-const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: auto auto auto;
-    grid-gap: 1rem;
-    padding: 0.5em;
-    align-items: center;
-    background-color: #2a6f97;
-    height: 12vh;
-    img {
-        max-height: 5rem;
-        cursor: pointer;
-        color: black;
-        transition: color 0.25s;
-    }
 
-    a {
-        color: white;
-        transition: color 0.25s;
-        :hover {
-            color: #ff6b6b;
-        }
-        text-decoration: none;
-        font-family: Courier New;
-    }
-`
-
-export default Footer
+export default Newfooter;
--- src/components/header.js
+++ src/components/header.js
@@ -1,71 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { useHistory as useHerstory } from 'react-router-dom'
-import {Link} from "react-router-dom"
-import ettermiddagen from '../img/ettermiddagen.svg'
-import ettermiddagenannenfarge from '../img/ettermiddagenannenfarge.svg'
 
-export const Info = ({ link1, text1, link2, text2, link3, text3 }) => {
-    const herstory = useHerstory()
-    const [logo, setLogo] = useState(ettermiddagen)
-    const toggleLogo = () => {
-        if (logo === ettermiddagen) {
-            setLogo(ettermiddagenannenfarge)
-        } else if (logo === ettermiddagenannenfarge) {
-            setLogo(ettermiddagen)
-        }
-    }
-    return (
-        <div>
-            <Wrapper>
-                <img
-                    onClick={() => herstory.push('/')}
-                    src={logo}
-                    alt="ettermiddagen@ifi logo"
-                    onMouseEnter={() => toggleLogo()}
-                    onMouseOut={() => toggleLogo()}
-                />
-                <span />
-                <Box>
-                    <Link to={`${link1}`}> {text1}</Link>
-                    <br />
-                    <Link to={`${link2}`}> {text2}</Link>
-                    <br />
-                    <Link to={`${link3}`}> {text3}</Link>
-                </Box>
-            </Wrapper>
+const newHeader = () => {
+    return(
+        <div className="header">
+                <span/>
+                <span/>
+                <Dagen src={require('../img/dagen_logo.svg')} alt="dagen@ifi logo" />
         </div>
     )
-}
+} 
 
-const Box = styled.div`
-    justify-self: right;
-    line-height: 2;
+const Dagen = styled.img`
+    right: 0px;
+    width: 200px;
+    height: 100px;
+    
 `
-
-const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: auto auto auto;
-    grid-gap: 1rem;
-    padding: 0.5em;
-    align-items: center;
-    background-color: #2a6f97;
-    img {
-        max-height: 5rem;
-        cursor: pointer;
-        color: black;
-        transition: color 0.25s;
-    }
-
-    a {
-        color: white;
-        transition: color 0.25s;
-        :hover {
-            color: #ff6b6b;
-        }
-        text-decoration: none;
-        font-family: Courier New;
-    }
-`
-
-export default Info
+export default newHeader;
